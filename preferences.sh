f0c1c5f0d58ac4b2d5eb84445e7f82e5e61f4148
Damir Tursunovic <hello@damir.me>
2021-03-21 21:28:52 +0100

    Update preferences.sh

--- preferences.sh
+++ preferences.sh
@@ -3,3 +3,7 @@ defaults write com.knollsoft.Rectangle rightHalf '{ keyCode = 124; modifierFlags
 defaults write com.knollsoft.Rectangle leftHalf '{ keyCode = 123; modifierFlags = 393216; }'
 defaults write com.knollsoft.Rectangle lastTwoThirds '{ keyCode = 124; modifierFlags = 917504; }'
 defaults write com.knollsoft.Rectangle firstThird '{ keyCode = 123; modifierFlags = 917504; }'
+
+# Ohhhhh yeahhh
+defaults write -g InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 1
